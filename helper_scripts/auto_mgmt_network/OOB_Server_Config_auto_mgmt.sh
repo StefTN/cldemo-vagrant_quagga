e0c7040d1250d222670feff41e91b0b9cde6cc5a
Eric Pulvino <ericpulvino@gmail.com>
2017-02-08 01:02:10 -0500

    Updated OOB scripts for use of Eth0 as vagrant interface.

--- helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
+++ helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
@@ -54,8 +54,8 @@ cat <<EOT > /etc/network/interfaces
 auto lo
 iface lo inet loopback
 
-auto vagrant
-iface vagrant inet dhcp
+auto eth0
+iface eth0 inet dhcp
 
 
 auto mgmt_net
@@ -174,7 +174,7 @@ echo " ###Making cumulus passwordless sudo ###"
 echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
 
 echo ' ### Setting UP NAT and Routing on MGMT server... ### '
-echo -e '#!/bin/bash \n/sbin/iptables -t nat -A POSTROUTING -o vagrant -j MASQUERADE' > /etc/rc.local
+echo -e '#!/bin/bash \n/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE' > /etc/rc.local
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/98-ipforward.conf
 
 
