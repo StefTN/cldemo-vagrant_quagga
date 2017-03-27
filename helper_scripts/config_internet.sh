16aea7879fcd155e42d7649a42457a591d8ae86f
Eric Pulvino <ericpulvino@gmail.com>
2017-03-27 13:19:00 -0700

    Updated and culled old helper scripts.

--- helper_scripts/config_internet.sh
+++ helper_scripts/config_internet.sh
@@ -28,15 +28,6 @@ echo -e "auto swp2" >> /etc/network/interfaces
 echo -e "iface swp2" >> /etc/network/interfaces
 
 
-## Convenience code. This is normally done in ZTP.
-echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
-mkdir -p /home/cumulus/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
-chmod 700 -R /home/cumulus/.ssh
-chown cumulus:cumulus -R /home/cumulus/.ssh
-echo "This is a fake license" > /etc/cumulus/.license.txt
-
-
 echo "#################################"
 echo "   Finished "
 echo "#################################"
