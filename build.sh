88df65ad10b0944ea1d74c00fda70467c1a623cc
Eric Pulvino <ericpulvino@gmail.com>
2017-05-02 18:53:23 -0400

    build.sh update and added a topology-centos.dot for the openstack sim.

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 python ./topology_converter.py -cmd ./topology.dot -p libvirt
 cp ./Vagrantfile ./Vagrantfile-kvm
 
+# Write Vagrantfile for Virtualbox (Centos)
+python ./topology_converter.py -cmd ./topology-centos.dot
+cp ./Vagrantfile ./Vagrantfile-centos
+
 # Write Vagrantfile for Virtualbox
 python ./topology_converter.py -cmd ./topology.dot
 cp ./Vagrantfile ./Vagrantfile-vbox
+
+
