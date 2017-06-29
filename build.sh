5d9eef080769d27a803ad598aebcd0cd20e305aa
Eric Pulvino <ericpulvino@gmail.com>
2017-06-29 13:54:32 -0400

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+apt-get install git python-pip
+sudo pip install pip --upgrade
+sudo pip install setuptools ipaddress pydotplus jinja2 --upgrade
+
+vagrant destroy -f
+wget https://raw.githubusercontent.com/CumulusNetworks/topology_converter/master/topology_converter.py
+mkdir -p ./templates/
+wget -O ./templates/Vagrantfile.j2 https://raw.githubusercontent.com/CumulusNetworks/topology_converter/master/templates/Vagrantfile.j2
+
 # Write Vagrantfile for Libvirt
 python ./topology_converter.py ./topology.dot -p libvirt
 cp ./Vagrantfile ./Vagrantfile-kvm
