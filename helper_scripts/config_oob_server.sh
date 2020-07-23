e550afddf704275c08cd479b596f433e09eada8a
Stefano Maurina <stefano.maurina@pm.me>
2020-07-23 22:41:44 +0200

    removing other scripts under script config_oob_server.sh

--- helper_scripts/config_oob_server.sh
+++ helper_scripts/config_oob_server.sh
@@ -297,248 +297,249 @@ EOT
 echo "############################################"
 echo "      DONE!"
 echo "############################################"
-#!/bin/bash
 
-echo "#################################"
-echo "   Running config_oob_switch.sh"
-echo "#################################"
-sudo su
+# #!/bin/bash
 
-# Config for OOB Switch
-cat <<EOT > /etc/network/interfaces
-auto lo
-iface lo inet loopback
+# echo "#################################"
+# echo "   Running config_oob_switch.sh"
+# echo "#################################"
+# sudo su
 
-auto eth0
-iface eth0 inet dhcp
-    alias Interface used by Vagrant
+# # Config for OOB Switch
+# cat <<EOT > /etc/network/interfaces
+# auto lo
+# iface lo inet loopback
 
-auto bridge
-iface bridge
-    alias Untagged Bridge
-    bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15
-    hwaddress a0:00:00:00:00:61
-    address 192.168.0.1/24
+# auto eth0
+# iface eth0 inet dhcp
+#     alias Interface used by Vagrant
+
+# auto bridge
+# iface bridge
+#     alias Untagged Bridge
+#     bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15
+#     hwaddress a0:00:00:00:00:61
+#     address 192.168.0.1/24
     
-EOT
+# EOT
 
-echo "#################################"
-echo "   Finished "
-echo "#################################"
+# echo "#################################"
+# echo "   Finished "
+# echo "#################################"
 
-#!/bin/bash
+# #!/bin/bash
 
-echo "#################################"
-echo "  Running config_server.sh"
-echo "#################################"
-sudo su
-
-
-#Replace existing network interfaces file
-echo -e "auto lo" > /etc/network/interfaces
-echo -e "iface lo inet loopback\n\n" >> /etc/network/interfaces
-
-#Add vagrant interface
-echo -e "\n\nauto eth0" >> /etc/network/interfaces
-echo -e "iface eth0 inet dhcp\n\n" >> /etc/network/interfaces
-
-useradd cumulus -m -s /bin/bash
-echo "cumulus:CumulusLinux!" | chpasswd
-sed "s/PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config
-
-## Convenience code. This is normally done in ZTP.
-echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
-mkdir /home/cumulus/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
-chmod 700 -R /home/cumulus
-chown -R cumulus:cumulus /home/cumulus
-chmod 600 /home/cumulus/.ssh/*
-chmod 700 /home/cumulus/.ssh
+# echo "#################################"
+# echo "  Running config_server.sh"
+# echo "#################################"
+# sudo su
 
 
-# Other stuff
-ping 8.8.8.8 -c2
-if [ "$?" == "0" ]; then
-  apt-get update -qy
-  apt-get install lldpd ntp ntpdate -qy
-  echo "configure lldp portidsubtype ifname" > /etc/lldpd.d/port_info.conf 
-fi
+# #Replace existing network interfaces file
+# echo -e "auto lo" > /etc/network/interfaces
+# echo -e "iface lo inet loopback\n\n" >> /etc/network/interfaces
 
-# Set Timezone
-cat << EOT > /etc/timezone
-Etc/UTC
-EOT
+# #Add vagrant interface
+# echo -e "\n\nauto eth0" >> /etc/network/interfaces
+# echo -e "iface eth0 inet dhcp\n\n" >> /etc/network/interfaces
 
-# Apply Timezone Now
-# dpkg-reconfigure -f noninteractive tzdata
+# useradd cumulus -m -s /bin/bash
+# echo "cumulus:CumulusLinux!" | chpasswd
+# sed "s/PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config
 
-# Write NTP Configuration
-cat << EOT > /etc/ntp.conf
-# /etc/ntp.conf, configuration for ntpd; see ntp.conf(5) for help
-
-driftfile /var/lib/ntp/ntp.drift
-
-statistics loopstats peerstats clockstats
-filegen loopstats file loopstats type day enable
-filegen peerstats file peerstats type day enable
-filegen clockstats file clockstats type day enable
-
-server 192.168.0.254 iburst
-
-# By default, exchange time with everybody, but don't allow configuration.
-restrict -4 default kod notrap nomodify nopeer noquery
-restrict -6 default kod notrap nomodify nopeer noquery
-
-# Local users may interrogate the ntp server more closely.
-restrict 127.0.0.1
-restrict ::1
-
-# Specify interfaces, don't listen on switch ports
-interface listen eth0
-EOT
-
-sudo systemctl enable ntp.service
-sudo systemctl start ntp.service
-
-echo "#################################"
-echo "   Finished"
-echo "#################################"
-#!/bin/bash
-
-echo "#################################"
-echo "  Running Switch Post Config (config_switch.sh)"
-echo "#################################"
-sudo su
+# ## Convenience code. This is normally done in ZTP.
+# echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
+# mkdir /home/cumulus/.ssh
+# echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
+# chmod 700 -R /home/cumulus
+# chown -R cumulus:cumulus /home/cumulus
+# chmod 600 /home/cumulus/.ssh/*
+# chmod 700 /home/cumulus/.ssh
 
 
-## Convenience code. This is normally done in ZTP.
+# # Other stuff
+# ping 8.8.8.8 -c2
+# if [ "$?" == "0" ]; then
+#   apt-get update -qy
+#   apt-get install lldpd ntp ntpdate -qy
+#   echo "configure lldp portidsubtype ifname" > /etc/lldpd.d/port_info.conf 
+# fi
+
+# # Set Timezone
+# cat << EOT > /etc/timezone
+# Etc/UTC
+# EOT
+
+# # Apply Timezone Now
+# # dpkg-reconfigure -f noninteractive tzdata
+
+# # Write NTP Configuration
+# cat << EOT > /etc/ntp.conf
+# # /etc/ntp.conf, configuration for ntpd; see ntp.conf(5) for help
+
+# driftfile /var/lib/ntp/ntp.drift
+
+# statistics loopstats peerstats clockstats
+# filegen loopstats file loopstats type day enable
+# filegen peerstats file peerstats type day enable
+# filegen clockstats file clockstats type day enable
+
+# server 192.168.0.254 iburst
+
+# # By default, exchange time with everybody, but don't allow configuration.
+# restrict -4 default kod notrap nomodify nopeer noquery
+# restrict -6 default kod notrap nomodify nopeer noquery
+
+# # Local users may interrogate the ntp server more closely.
+# restrict 127.0.0.1
+# restrict ::1
+
+# # Specify interfaces, don't listen on switch ports
+# interface listen eth0
+# EOT
+
+# sudo systemctl enable ntp.service
+# sudo systemctl start ntp.service
+
+# echo "#################################"
+# echo "   Finished"
+# echo "#################################"
+# #!/bin/bash
+
+# echo "#################################"
+# echo "  Running Switch Post Config (config_switch.sh)"
+# echo "#################################"
+# sudo su
+
+
+# ## Convenience code. This is normally done in ZTP.
 
 
 
-echo "#################################"
-echo "   Finished"
-echo "#################################"
-#!/bin/bash
+# echo "#################################"
+# echo "   Finished"
+# echo "#################################"
+# #!/bin/bash
 
-echo "#################################"
-echo "   Running config_oob_switch.sh"
-echo "#################################"
-sudo su
+# echo "#################################"
+# echo "   Running config_oob_switch.sh"
+# echo "#################################"
+# sudo su
 
-# Config for OOB Switch
-cat <<EOT > /etc/network/interfaces
-auto lo
-iface lo inet loopback
+# # Config for OOB Switch
+# cat <<EOT > /etc/network/interfaces
+# auto lo
+# iface lo inet loopback
 
-auto eth0
-iface eth0 inet dhcp
-    alias Interface used by Vagrant
+# auto eth0
+# iface eth0 inet dhcp
+#     alias Interface used by Vagrant
 
-auto bridge
-iface bridge
-    alias Untagged Bridge
-    bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15
-    hwaddress a0:00:00:00:00:61
-    address 192.168.0.1/24
+# auto bridge
+# iface bridge
+#     alias Untagged Bridge
+#     bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15
+#     hwaddress a0:00:00:00:00:61
+#     address 192.168.0.1/24
     
-EOT
+# EOT
 
-echo "#################################"
-echo "   Finished "
-echo "#################################"
+# echo "#################################"
+# echo "   Finished "
+# echo "#################################"
 
-#!/bin/bash
+# #!/bin/bash
 
-echo "#################################"
-echo "  Running config_server.sh"
-echo "#################################"
-sudo su
-
-
-#Replace existing network interfaces file
-echo -e "auto lo" > /etc/network/interfaces
-echo -e "iface lo inet loopback\n\n" >> /etc/network/interfaces
-
-#Add vagrant interface
-echo -e "\n\nauto eth0" >> /etc/network/interfaces
-echo -e "iface eth0 inet dhcp\n\n" >> /etc/network/interfaces
-
-useradd cumulus -m -s /bin/bash
-echo "cumulus:CumulusLinux!" | chpasswd
-sed "s/PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config
-
-## Convenience code. This is normally done in ZTP.
-echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
-mkdir /home/cumulus/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
-chmod 700 -R /home/cumulus
-chown -R cumulus:cumulus /home/cumulus
-chmod 600 /home/cumulus/.ssh/*
-chmod 700 /home/cumulus/.ssh
+# echo "#################################"
+# echo "  Running config_server.sh"
+# echo "#################################"
+# sudo su
 
 
-# Other stuff
-ping 8.8.8.8 -c2
-if [ "$?" == "0" ]; then
-  apt-get update -qy
-  apt-get install lldpd ntp ntpdate -qy
-  echo "configure lldp portidsubtype ifname" > /etc/lldpd.d/port_info.conf 
-fi
+# #Replace existing network interfaces file
+# echo -e "auto lo" > /etc/network/interfaces
+# echo -e "iface lo inet loopback\n\n" >> /etc/network/interfaces
 
-# Set Timezone
-cat << EOT > /etc/timezone
-Etc/UTC
-EOT
+# #Add vagrant interface
+# echo -e "\n\nauto eth0" >> /etc/network/interfaces
+# echo -e "iface eth0 inet dhcp\n\n" >> /etc/network/interfaces
 
-# Apply Timezone Now
-# dpkg-reconfigure -f noninteractive tzdata
+# useradd cumulus -m -s /bin/bash
+# echo "cumulus:CumulusLinux!" | chpasswd
+# sed "s/PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config
 
-# Write NTP Configuration
-cat << EOT > /etc/ntp.conf
-# /etc/ntp.conf, configuration for ntpd; see ntp.conf(5) for help
-
-driftfile /var/lib/ntp/ntp.drift
-
-statistics loopstats peerstats clockstats
-filegen loopstats file loopstats type day enable
-filegen peerstats file peerstats type day enable
-filegen clockstats file clockstats type day enable
-
-server 192.168.0.254 iburst
-
-# By default, exchange time with everybody, but don't allow configuration.
-restrict -4 default kod notrap nomodify nopeer noquery
-restrict -6 default kod notrap nomodify nopeer noquery
-
-# Local users may interrogate the ntp server more closely.
-restrict 127.0.0.1
-restrict ::1
-
-# Specify interfaces, don't listen on switch ports
-interface listen eth0
-EOT
-
-sudo systemctl enable ntp.service
-sudo systemctl start ntp.service
-
-echo "#################################"
-echo "   Finished"
-echo "#################################"
-#!/bin/bash
-
-echo "#################################"
-echo "  Running Switch Post Config (config_switch.sh)"
-echo "#################################"
-sudo su
+# ## Convenience code. This is normally done in ZTP.
+# echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
+# mkdir /home/cumulus/.ssh
+# echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
+# chmod 700 -R /home/cumulus
+# chown -R cumulus:cumulus /home/cumulus
+# chmod 600 /home/cumulus/.ssh/*
+# chmod 700 /home/cumulus/.ssh
 
 
-## Convenience code. This is normally done in ZTP.
+# # Other stuff
+# ping 8.8.8.8 -c2
+# if [ "$?" == "0" ]; then
+#   apt-get update -qy
+#   apt-get install lldpd ntp ntpdate -qy
+#   echo "configure lldp portidsubtype ifname" > /etc/lldpd.d/port_info.conf 
+# fi
+
+# # Set Timezone
+# cat << EOT > /etc/timezone
+# Etc/UTC
+# EOT
+
+# # Apply Timezone Now
+# # dpkg-reconfigure -f noninteractive tzdata
+
+# # Write NTP Configuration
+# cat << EOT > /etc/ntp.conf
+# # /etc/ntp.conf, configuration for ntpd; see ntp.conf(5) for help
+
+# driftfile /var/lib/ntp/ntp.drift
+
+# statistics loopstats peerstats clockstats
+# filegen loopstats file loopstats type day enable
+# filegen peerstats file peerstats type day enable
+# filegen clockstats file clockstats type day enable
+
+# server 192.168.0.254 iburst
+
+# # By default, exchange time with everybody, but don't allow configuration.
+# restrict -4 default kod notrap nomodify nopeer noquery
+# restrict -6 default kod notrap nomodify nopeer noquery
+
+# # Local users may interrogate the ntp server more closely.
+# restrict 127.0.0.1
+# restrict ::1
+
+# # Specify interfaces, don't listen on switch ports
+# interface listen eth0
+# EOT
+
+# sudo systemctl enable ntp.service
+# sudo systemctl start ntp.service
+
+# echo "#################################"
+# echo "   Finished"
+# echo "#################################"
+# #!/bin/bash
+
+# echo "#################################"
+# echo "  Running Switch Post Config (config_switch.sh)"
+# echo "#################################"
+# sudo su
+
+
+# ## Convenience code. This is normally done in ZTP.
 
 
 
-echo "#################################"
-echo "   Finished"
-echo "#################################"
+# echo "#################################"
+# echo "   Finished"
+# echo "#################################"
 
 cat << EOT >> /home/cumulus/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDee1uZVFHxptB43LP8dSR5rNjqmPhuzwYbB1KgDTbssc7fha4lB9rDLSjuiAGWSM3iq1RHrpzED0uTMc9i0wawDmPb2TglELIrPXas2mpx5c/j7GnjypMAX3NhBimKq5jZR d+AmfE4Y1bTl0nKeWjucZDgwnpP1x6gcR2xGDZzy1HHAHDP0JvE8QCbxKq/SgMl7nXE+j1cvXxTy9sjF4OBLIKtdw7P/Qqkc3GfsgzKSgSNp/s/crmr5SJ7fv2s20nZCw81hgJZ3orNpPRsFFAtRbP6QnX4o8JQVyJAEVVlb0gQ byKuTDmZj/FFhq46SYqo5kUx0loaYns9dPmitfy5 stefano@Ubuntu-VirtualBox
